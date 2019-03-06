c2305f962c9172e14571a8c1e78a8a44fbb47be7
byronholmes2018 <lshopeful2018@mgmail.com>
2019-03-06 06:14:33 -0600

    Completed MVP

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,50 @@
 // Test away!
+import React from 'react'
+import { render, fireEvent } from 'react-testing-library'
+import 'jest-dom/extend-expect'
+import 'react-testing-library/cleanup-after-each'
+import Controls from './Controls'
+
+describe('<Controls />', ()=>{
+  it('closed toggle button should call the passed function', ()=>{
+    const mockFn = jest.fn()
+    const { getByText } = render(<Controls toggleClosed={mockFn} />)
+    const button = getByText(/close gate/i)
+
+    fireEvent.click(button)
+
+    expect(mockFn).toHaveBeenCalledTimes(1)
+  })
+})
+
+it('locked toggle button should call the passed function', ()=>{
+  const mockFn = jest.fn()
+  const { getByText } = render(<Controls toggleLocked = {mockFn} closed />)
+  const button = getByText(/lock gate/i)
+
+  fireEvent.click(button)
+
+  expect(mockFn).toHaveBeenCalledTimes(1)
+})
+
+it('should disable locked toggle button if the gate is open', ()=>{
+  const mockFn = jest.fn()
+  const { getByText } = render(
+    <Controls toggleLocked={mockFn} closed={false} />  )
+  const button = getByText(/lock gate/i)
+
+  expect(button).toBeDisabled()
+  fireEvent.click(button)
+  expect(mockFn).not.toHaveBeenCalled()
+})
+
+it('should disable closed toggle button if gate is locked', ()=>{
+  const mockFn = jest.fn()
+  const { getByText } = render(
+    <Controls toggleLocked={mockFn} locked={true} />  )
+  const button = getByText(/close gate/i)
+
+  expect(button).toBeDisabled()
+  fireEvent.click(button)
+  expect(mockFn).not.toHaveBeenCalled()
+})
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,37 @@
 // Test away!
+import React from 'react'
+import { render, fireEvent } from 'react-testing-library'
+import 'jest-dom/extend-expect'
+import 'react-testing-library/cleanup-after-each'
+
+import Display from './Display'
+
+describe('<Display />', ()=>{
+  it('should use red-led class when locked', ()=>{
+    const { getByText } = render(<Display locked={true} />)
+
+    const lockedDisplay = getByText(/locked/i)
+    expect(lockedDisplay).toHaveClass('red-led')
+    
+  })
+  it('should use green-led class when unlocked', ()=>{
+    const { getByText } = render(<Display locked = {false} />)
+    const lockedDisplay = getByText(/unlocked/i)
+    expect(lockedDisplay).toHaveClass('green-led')
+
+  })
+  it('should use green-led class when opened', ()=>{
+    const { getByText } = render(<Display closed = {false} />)
+
+    const openDisplay = getByText(/open/i)
+    expect(openDisplay).toHaveClass('green-led')
+  })
+
+  it('should use red-led class when closed', ()=>{
+    const { getByText } = render(<Display closed = {true} />)
+
+    const openDisplay = getByText(/closed/i)
+    expect(openDisplay).toHaveClass('red-led')
+  })
+
+})
